0e1c6aabfed2a446e9ef59a9d7986c0ceaf4aed8
sajidCode-gh <muhammadsajidkhan616@gmail.com>
2024-06-09 16:26:09 +0500

    some new fancy keymaps

--- lua/general-keymaps.lua
+++ lua/general-keymaps.lua
@@ -1,4 +1,3 @@
-
 vim.keymap.set("i", "jk", "<Esc>", {})
 
 -- copy in Visula mode
@@ -30,11 +29,8 @@ vim.keymap.set("i", "<A-k>", "<esc><cmd>m .2<cr>==gi", { desc = "Move Up" })
 vim.keymap.set("v", "<A-j>", ":m '>+5<cr>gv=gv", { desc = "Move Down" })
 vim.keymap.set("v", "<A-k>", ":m '<2<cr>gv=gv", { desc = "Move Up" })
 
-
 -- Clear search with <esc>
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
-
-
 
 -- save file
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
@@ -53,8 +49,9 @@ vim.keymap.set("n", "<leader>l", ":Lazy<cr>", { desc = "Lazy" })
 vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
 -- LazyVim Changelog
-vim.keymap.set("n", "<leader>L", function() LazyVim.news.changelog() end, { desc = "LazyVim Changelog" })
-
+vim.keymap.set("n", "<leader>L", function()
+	LazyVim.news.changelog()
+end, { desc = "LazyVim Changelog" })
 
 -- windows
 vim.keymap.set("n", "<leader>ww", "<C-W>p", { desc = "Other Window", remap = true })
@@ -63,17 +60,36 @@ vim.keymap.set("n", "<leader>w-", "<C-W>s", { desc = "Split Window Below", remap
 vim.keymap.set("n", "<leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
 vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 vim.keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
-vim.keymap.set("n", "<leader>wm", function() LazyVim.toggle.maximize() end, { desc = "Maximize Toggle" })
-vim.keymap.set("n", "<leader>m", function() LazyVim.toggle.maximize() end, { desc = "Maximize Toggle" })
-
--- tabs
-vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-vim.keymap.set("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
-vim.keymap.set("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
-vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+vim.keymap.set("n", "<leader>wm", function()
+	LazyVim.toggle.maximize()
+end, { desc = "Maximize Toggle" })
+vim.keymap.set("n", "<leader>m", function()
+	LazyVim.toggle.maximize()
+end, { desc = "Maximize Toggle" })
 
 -- select all
 vim.keymap.set("n", "<C-a>", "ggVG", {})
+
+-- keybinding to prevent yanking while changing or Deleting text.
+vim.keymap.set("n", "dd", '"_dd', { noremap = true, silent = true })
+vim.keymap.set("v", "d", '"_d', { noremap = true, silent = true })
+vim.keymap.set("n", "cc", '"_cc', { noremap = true, silent = true })
+vim.keymap.set("v", "c", '"_c', { noremap = true, silent = true })
+vim.keymap.set("n", "p", "p", { noremap = true, silent = true })
+vim.keymap.set("v", "p", '"_dP', { noremap = true, silent = true })
+
+--when you highlight, you can move lines. in highlight mode try shift+j/k to move
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+
+-- navigate through page with halp page length and remain curson in the middle.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+--search but keep the curson in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- use O or o to crate new lines without transition into insert mode.
+vim.keymap.set("n", "<leader>o", "o<esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>O", "O<esc>", { noremap = true, silent = true })
