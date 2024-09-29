39b7befa636b5ef3959ab12914ae7487854c7abc
sajidcodess <muhammadsajidkhan616@gmail.com>
2024-09-30 01:34:30 +0500

    add borders to hover popup

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -65,6 +65,16 @@ return {
 			lspconfig.gopls.setup({
 				capabilities = capabilities,
 			})
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
+			vim.diagnostic.config({
+				float = {
+					border = "rounded",
+				},
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
