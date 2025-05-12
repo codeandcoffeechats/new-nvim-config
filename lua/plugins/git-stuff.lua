77ecb2c3d3206fae803ea966f8286fbc54c0d2f3
sajidcodesdotcom <muhammadsajidkhan616@gmail.com>
2025-05-12 16:22:38 +0500

    keybindings for vim fugitive

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,6 +1,17 @@
 return {
   {
     "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>gs", ":G<CR>", { desc = "Git Status" })
+      vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git Blame" })
+      vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { desc = "Git Diff" })
+      vim.keymap.set("n", "<leader>gc", ":Gcommit<CR>", { desc = "Git Commit" })
+      vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git Push" })
+      vim.keymap.set("n", "<leader>gl", ":Git log<CR>", { desc = "Git Log" })
+      vim.keymap.set("n", "<leader>gco", ":Git checkout<CR>", { desc = "Git Checkout" })
+      vim.keymap.set("n", "<leader>gw", ":Gwrite<CR>", { desc = "Git Add (Stage File)" })
+      vim.keymap.set("n", "<leader>gr", ":Gremove<CR>", { desc = "Git Remove (Stage File)" })
+    end,
   },
   {
     "lewis6991/gitsigns.nvim",
