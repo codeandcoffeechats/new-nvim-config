870dfc8e1c4a4c4f6873636c037cf3e8f522228e
sajidcodesdotcom <muhammadsajidkhan616@gmail.com>
2025-05-14 20:12:51 +0500

    format fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,106 +1,128 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-    ensure_installed = {
-      "pyright",
-      "biome",
-      "jsonls",
-      "marksman",
-      "tailwindcss",
-      "cssls",
-      "eslint",
-      "ts_ls",
-      "solargraph",
-      "html",
-      "lua_ls",
-      "gopls",
-      "sqlls",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		ensure_installed = {
+			"pyright",
+			"biome",
+			"jsonls",
+			"marksman",
+			"tailwindcss",
+			"cssls",
+			"eslint",
+			"ts_ls",
+			"solargraph",
+			"html",
+			"lua_ls",
+			"gopls",
+			"sqlls",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      local servers = {
-        "pyright",
-        "biome",
-        "jsonls",
-        "marksman",
-        "tailwindcss",
-        "cssls",
-        "eslint",
-        "ts_ls",
-        "solargraph",
-        "html",
-        "lua_ls",
-        "gopls",
-        "sqlls",
-      }
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-        })
-      end
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+			local lspconfig = require("lspconfig")
+			local servers = {
+				"pyright",
+				"biome",
+				"jsonls",
+				"marksman",
+				"tailwindcss",
+				"cssls",
+				"eslint",
+				"ts_ls",
+				"solargraph",
+				"html",
+				"lua_ls",
+				"gopls",
+				"sqlls",
+			}
+			for _, server in ipairs(servers) do
+				lspconfig[server].setup({
+					capabilities = capabilities,
+				})
+			end
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
-      vim.lsp.handlers["textDocument/signatureHelp"] =
-          vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
-      vim.diagnostic.config({
-        float = {
-          border = "rounded",
-        },
-      })
+			vim.diagnostic.config({
+				float = {
+					border = "rounded",
+				},
+				signs = true,
+				underline = false,
+				virtual_text = true,
+			})
 
-      vim.keymap.set('n', 'grn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
+			vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
 
-      -- Execute a code action, usually your cursor needs to be on top of an error
-      -- or a suggestion from your LSP for this to activate.
-      vim.keymap.set('n', 'gra', vim.lsp.buf.code_action, { desc = '[G]oto Code [A]ction' })
+			-- Execute a code action, usually your cursor needs to be on top of an error
+			-- or a suggestion from your LSP for this to activate.
+			vim.keymap.set("n", "gra", vim.lsp.buf.code_action, { desc = "[G]oto Code [A]ction" })
 
-      -- Find references for the word under your cursor.
-      vim.keymap.set('n', 'grr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+			-- Find references for the word under your cursor.
+			vim.keymap.set("n", "grr", require("telescope.builtin").lsp_references, { desc = "[G]oto [R]eferences" })
 
-      -- Jump to the implementation of the word under your cursor.
-      --  Useful when your language has ways of declaring types without an actual implementation.
-      vim.keymap.set('n', 'gri', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
+			-- Jump to the implementation of the word under your cursor.
+			--  Useful when your language has ways of declaring types without an actual implementation.
+			vim.keymap.set(
+				"n",
+				"gri",
+				require("telescope.builtin").lsp_implementations,
+				{ desc = "[G]oto [I]mplementation" }
+			)
 
-      -- Jump to the definition of the word under your cursor.
-      --  This is where a variable was first declared, or where a function is defined, etc.
-      --  To jump back, press <C-t>.
-      vim.keymap.set('n', 'grd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
+			-- Jump to the definition of the word under your cursor.
+			--  This is where a variable was first declared, or where a function is defined, etc.
+			--  To jump back, press <C-t>.
+			vim.keymap.set("n", "grd", require("telescope.builtin").lsp_definitions, { desc = "[G]oto [D]efinition" })
 
-      -- WARN: This is not Goto Definition, this is Goto Declaration.
-      --  For example, in C this would take you to the header.
-      vim.keymap.set('n', 'grD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
+			-- WARN: This is not Goto Definition, this is Goto Declaration.
+			--  For example, in C this would take you to the header.
+			vim.keymap.set("n", "grD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" })
 
-      -- Fuzzy find all the symbols in your current document.
-      --  Symbols are things like variables, functions, types, etc.
-      vim.keymap.set('n', 'gO', require('telescope.builtin').lsp_document_symbols, { desc = 'Open Document Symbols' })
+			-- Fuzzy find all the symbols in your current document.
+			--  Symbols are things like variables, functions, types, etc.
+			vim.keymap.set(
+				"n",
+				"gO",
+				require("telescope.builtin").lsp_document_symbols,
+				{ desc = "Open Document Symbols" }
+			)
 
-      -- Fuzzy find all the symbols in your current workspace.
-      --  Similar to document symbols, except searches over your entire project.
-      vim.keymap.set('n', 'gW', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-        { desc = 'Open Workspace Symbols' })
+			-- Fuzzy find all the symbols in your current workspace.
+			--  Similar to document symbols, except searches over your entire project.
+			vim.keymap.set(
+				"n",
+				"gW",
+				require("telescope.builtin").lsp_dynamic_workspace_symbols,
+				{ desc = "Open Workspace Symbols" }
+			)
 
-      -- Jump to the type of the word under your cursor.
-      --  Useful when you're not sure what type a variable is and you want to see
-      --  the definition of its *type*, not where it was *defined*.
-      vim.keymap.set('n', 'grt', require('telescope.builtin').lsp_type_definitions, { desc = '[G]oto [T]ype Definition' })
-    end,
-  },
+			-- Jump to the type of the word under your cursor.
+			--  Useful when you're not sure what type a variable is and you want to see
+			--  the definition of its *type*, not where it was *defined*.
+			vim.keymap.set(
+				"n",
+				"grt",
+				require("telescope.builtin").lsp_type_definitions,
+				{ desc = "[G]oto [T]ype Definition" }
+			)
+		end,
+	},
 }
