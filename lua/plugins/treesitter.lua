1a4532b13b616ffa3828701bee8c49e9c6f2a3de
sajidcodesdotcom <muhammadsajidkhan616@gmail.com>
2025-05-14 15:31:47 +0500

    we love treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,16 @@
-return { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
+return {
+	"nvim-treesitter/nvim-treesitter",
+	run = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
 			auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end
+			sync_install = false,
+			ensure_installed = { "lua", "python", "javascript", "typescript", "html", "css", "sql", "go" }, -- Add parsers you want to ensure are installed
+			ignore_install = {}, -- List parsers to ignore during installation
+			modules = {}, -- Additional modules can be added here
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
