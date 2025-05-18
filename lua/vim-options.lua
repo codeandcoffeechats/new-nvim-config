346541c746738716b0c83e98176df74b13fc3a6b
sajidcodesdotcom <muhammadsajidkhan616@gmail.com>
2025-05-18 16:48:46 +0500

    c for completion and a for autopairs hehe

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,15 +3,15 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
-vim.cmd("set number") -- Ensure the current line shows its absolute number
+vim.cmd("set number") -- ensure the current line shows its absolute number
 
---  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+--  see `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("textyankpost", {
+	desc = "highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 vim.g.mapleader = " "
@@ -27,7 +27,7 @@ vim.opt.laststatus = 3
 
 vim.o.signcolumn = "yes"
 vim.opt.cursorline = true
-vim.opt.cursorlineopt = "number"
+-- vim.opt.cursorlineopt = "number" -- It will remove the highlight on the current/active line
 
 vim.opt.termguicolors = true
 
@@ -36,16 +36,16 @@ vim.o.scrolloff = 5
 
 -- Add clickboard support
 if vim.fn.has("wsl") == 1 then
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
 end
