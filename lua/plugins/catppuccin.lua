0eef66a1d8c6219a9c4490dee8d5e6c11cd00a26
sajidcodesdotcom <muhammadsajidkhan616@gmail.com>
2025-05-26 20:28:17 +0500

    format wit conform

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,19 +1,19 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "auto",
-      transparent_background = true,
-      highlight_overrides = {
-        all = function(colors)
-          return {
-            CursorLineNr = { fg = colors.peach },
-          }
-        end,
-      },
-    })
-    vim.cmd.colorscheme("catppuccin")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "auto",
+			transparent_background = true,
+			highlight_overrides = {
+				all = function(colors)
+					return {
+						CursorLineNr = { fg = colors.peach },
+					}
+				end,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
