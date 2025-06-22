3dd81309891003e980a300ee4d33e08f4b205da1
sajidcodesdotcom <muhammadsajidkhan616@gmail.com>
2025-06-23 02:34:15 +0500

    disable auto signature help openjk

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -26,6 +26,9 @@ return {
 					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
+				signature = {
+					enabled = false, -- enable signature help
+				},
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
