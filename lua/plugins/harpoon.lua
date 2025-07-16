bdf36b3bf25fde79998dfff9ec82b2ce3363e1ef
Muhammad Sajid <sajidcodes@Muhammads-MacBook-Pro.local>
2025-07-17 02:51:24 +0500

    conflicting keybinding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,30 +1,30 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
 
-		harpoon:setup()
+    harpoon:setup()
 
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-		end, { desc = "Add file to Harpoon" })
-		vim.keymap.set("n", "<leader>h", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end, { desc = "Toggle Harpoon quick menu" })
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+    end, { desc = "Add file to Harpoon" })
+    vim.keymap.set("n", "<leader>h", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Toggle Harpoon quick menu" })
 
-		vim.keymap.set("n", "<C-h>", function()
-			harpoon:list():select(1)
-		end, { desc = "Select 1st file in Harpoon" })
-		vim.keymap.set("n", "<C-t>", function()
-			harpoon:list():select(2)
-		end, { desc = "Select 2nd file in Harpoon" })
-		vim.keymap.set("n", "<C-n>", function()
-			harpoon:list():select(3)
-		end, { desc = "Select 3rd file in Harpoon" })
-		vim.keymap.set("n", "<C-s>", function()
-			harpoon:list():select(4)
-		end, { desc = "Select 4th file in Harpoon" })
-	end,
+    -- vim.keymap.set("n", "<C-h>", function()
+    -- 	harpoon:list():select(1)
+    -- end, { desc = "Select 1st file in Harpoon" })
+    -- vim.keymap.set("n", "<C-t>", function()
+    -- 	harpoon:list():select(2)
+    -- end, { desc = "Select 2nd file in Harpoon" })
+    -- vim.keymap.set("n", "<C-n>", function()
+    -- 	harpoon:list():select(3)
+    -- end, { desc = "Select 3rd file in Harpoon" })
+    -- vim.keymap.set("n", "<C-s>", function()
+    -- 	harpoon:list():select(4)
+    -- end, { desc = "Select 4th file in Harpoon" })
+  end,
 }
